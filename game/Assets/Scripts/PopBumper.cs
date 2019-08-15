d0d4b3f609ae78bec15b3cbf480104fb7bda0215
Jacob Hagstedt <jacob.hagstedt@lamneasoftware.se>
2019-08-15 20:54:54 +0200

    Add script for pop bumper

---
+++ game/Assets/Scripts/PopBumper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopBumper : MonoBehaviour
+{
+    public float PushBackForce;
+
+    Vector3 PopBumberLocation;
+    // public AudioSource HitSound;
+    void Start()
+    {
+        PopBumberLocation = gameObject.transform.position;
+        // HitSound = GetComponent<AudioSource>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            Rigidbody ball = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 forceVector = (ball.position - PopBumberLocation).normalized * PushBackForce;
+
+            ball.AddForce(forceVector);
+            // HitSound.Play();
+        }
+    }
+}
