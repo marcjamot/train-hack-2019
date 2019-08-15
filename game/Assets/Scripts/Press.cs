2e9fccbb84b28d972598edc67493a649d9229433
Marcus Hultman <marcus.hultman1@gmail.com>
2019-08-15 17:45:45 +0200

    pressss

---
+++ game/Assets/Scripts/Press.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class press : MonoBehaviour
+{
+    
+    
+
+    float _current_amount;
+
+    public KeyCode key = KeyCode.None;
+    public float rest = 110.0f;
+    public float reach = -50.0f;
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(key)) {
+            _current_amount = Mathf.Min(1, _current_amount + speed * Time.deltaTime);
+        } else {
+            _current_amount = Mathf.Max(0, _current_amount - speed * Time.deltaTime);
+        }
+        var angle = Mathf.Lerp(rest, rest + reach, _current_amount);
+        var euler = transform.localEulerAngles;
+        Debug.Log(euler.y);
+        // transform.localEulerAngles = new Vector3(euler.x, angle, euler.z);
+    }
+}
