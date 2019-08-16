a7da8422c303d817a93130cef8e452d056103356
Marcus Hultman <marcus.hultman1@gmail.com>
2019-08-16 09:46:54 +0200

    add world_generator script

---
+++ game/Assets/Scripts/world_generator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class world_generator : MonoBehaviour
+{
+    const float kTileWidth = 2f;
+    Vector3 kPlaneScale = Vector3.one * 0.2f;
+
+    public class Stop {
+        public string name = "stop ";
+        public Vector3 position;
+  }
+
+    List<Stop> stops = new List<Stop>();
+
+    public GameObject tile;
+
+    public int tiles = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var i = 0; i < tiles; ++i) {
+            var stop = new Stop();
+            stop.name += i;
+            stop.position = new Vector3(0, 0, i * kTileWidth);
+            stops.Add(stop);
+        }
+        foreach (var stop in stops) {
+            var clone = Object.Instantiate(tile, stop.position, Quaternion.identity);
+            clone.transform.SetParent(transform);
+        }
+        // Right plane
+        var rightPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        rightPlane.transform.position = new Vector3(kTileWidth * (tiles + 1) / 2, 0, kTileWidth * (tiles - 1f) / 2);
+        rightPlane.transform.localScale = tiles * kPlaneScale;
+        rightPlane.transform.SetParent(transform);
+        // Left plane
+        var leftPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        leftPlane.transform.position = new Vector3(kTileWidth * (-tiles - 1) / 2, 0, kTileWidth * (tiles - 1f) / 2);
+        leftPlane.transform.localScale = tiles * kPlaneScale;
+        leftPlane.transform.SetParent(transform);
+        // ...and beyond
+        var beyondPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        beyondPlane.transform.position = new Vector3(0, 0, 2 * kTileWidth * tiles);
+        beyondPlane.transform.localScale = 2 * (tiles + 0.5f) * kPlaneScale;
+        beyondPlane.transform.SetParent(transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
