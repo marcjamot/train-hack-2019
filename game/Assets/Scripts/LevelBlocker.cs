0d063fdcf1f5569c78f38389cafd6a710540b6ac
Jacob Hagstedt <jacob.hagstedt@lamneasoftware.se>
2019-08-17 09:07:56 +0200

    Update level blocker

--- game/Assets/Scripts/LevelBlocker.cs
+++ game/Assets/Scripts/LevelBlocker.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class LevelBlocker : MonoBehaviour
 {
     public float BottomPosition;
+    private float StartPosition = 0.17f;
     public float MoveForce;
+    public float TriggerTime;
+    private bool CanMoveDown = true;
 
     void Update()
     {
-        if (transform.position.y > BottomPosition)
+        if (CanMoveDown && transform.position.y > BottomPosition)
         {
             transform.position += Vector3.down * MoveForce * Time.deltaTime;
+            CanMoveDown = transform.position.y > BottomPosition;
+        }
+
+        if (!CanMoveDown)
+        {
+            // start a timer, when that one triggers, move the box up again
+            TriggerTime -= Time.deltaTime;
+            if (TriggerTime < 0f && transform.position.y <= StartPosition)
+            {
+                Debug.Log("Opening new level");
+                transform.position += Vector3.up * MoveForce * Time.deltaTime;
+            }
         }
     }
 }
